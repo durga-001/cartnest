baf1435edeb3c22358148f398c8e06a1e8eb44c8
Kanak <rathorkanak03@gmail.com>
2026-08-01 14:09:43 +0530

    authorization issue

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,24 +1,32 @@
-import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay} from '../controllers//orderController.js'
-import adminAuth from '../middleware/adminAuth.js'
-import authUser from '../middleware/auth.js'
+import express from "express";
+import {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+  verifyStripe,
+  verifyRazorpay,
+} from "../controllers//orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
 
-const orderRouter = express.Router()
+const orderRouter = express.Router();
 
 //admin features
-orderRouter.post('list',adminAuth , allOrders)
-orderRouter.post('/status',adminAuth, updateStatus)
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
 
 //payment features
-orderRouter.post('/place',authUser,placeOrder)
-orderRouter.post('/stripe',authUser,placeOrderStripe)
-orderRouter.post('/razorpay',authUser, placeOrderRazorpay)
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, placeOrderRazorpay);
 
 //user feature
-orderRouter.post('/userorders',authUser, userOrders)
+orderRouter.post("/userorders", authUser, userOrders);
 
-orderRouter.post('verifyStripe', authUser, verifyStripe)
-orderRouter.post('verifyRazorpay', authUser, verifyRazorpay)
+orderRouter.post("/verifyStripe", authUser, verifyStripe);
+orderRouter.post("/verifyRazorpay", authUser, verifyRazorpay);
 
-
-export default orderRouter
+export default orderRouter;
