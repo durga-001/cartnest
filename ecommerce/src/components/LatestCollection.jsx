7c95b06e825617b79cead4337fe24c3d46d8580f
Kanak <rathorkanak03@gmail.com>
2026-08-04 12:59:21 +0530

    data added

--- ecommerce/src/components/LatestCollection.jsx
+++ ecommerce/src/components/LatestCollection.jsx
@@ -15,8 +15,8 @@ const LatestCollection = () => {
       <div className="text-center py-8 text-3xl">
         <Title text1={"Latest"} text2={"Collection"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam
-          saepe praesentium quibusdam.
+          CartNest is your trusted hub for discovering and shopping unique
+          artisan products.
         </p>
       </div>
       {/*Rendering Product */}
